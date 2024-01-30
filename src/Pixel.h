664c20a2b04b89dc02e7debc92536618c49047e2
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-30 11:58:45 +0100

    update pixel.h

--- src/Pixel.h
+++ src/Pixel.h
@@ -1,13 +1,10 @@
 #ifndef PIXEL H
 #define _PIXEL_H
 
-class Pixel {
- private:
+struct Pixel {
   unsigned char r, g, b;
-
- public:
-  Pixel();
-  Pixel(int nr, int ng, int nb);
+  Pixel() : r(0), g(0), b(0){};
+  Pixel(int nr, int ng, int nb) : r(nr), g(ng), b(nb){};
 };
 
 #endif
